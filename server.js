204d2cbe0e689230dfb90ce5c5737f28cbf9f981
Christopher Bahinting <bahintingchristopher@gmail.com>
2026-07-02 19:59:11 +0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import {fileURLToPath} from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://127.0.0.1:${PORT}`);
+  console.log(`Environment: ${NODE_ENV}`);
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', async (req, res) => {
+  const title = 'Home';
+  res.render('home', { title });
+});
+ 
+app.get('/organizations', async (req, res) => {
+    const title = 'our Partner Organizations';
+    res.render('organizations', { title });
+});
+
+app.get('/projects', async (req, res) => {
+    const title = 'Service Projects';
+    res.render('projects', { title });
+});
+
+//set ejs as the templating engine
+app.set('view engine', 'ejs');
+
+//tell express where to find the ejs templates
+app.set('views', path.join(__dirname, 'src/views'));
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://127.0.0.1:${PORT}`);
+  console.log(`Environment: ${NODE_ENV}`);
+});
